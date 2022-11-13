98a92a34c720623960c1ec250fbeaafbecbf1cae
Ekaterina Kolpakova <kolpakovapost@yandex.ru>
2022-11-13 13:05:21 +0300

    Add Example 011 Method Array

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//Имеется массив из n элементов, требуется найти элемент массива равный find
+
+int[] array = { 1, 12, 31, 18, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index);
+        break;
+    }
+    index++;
+}
